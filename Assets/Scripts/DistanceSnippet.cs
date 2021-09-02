f70684ff8e7d6e0d22d6ad291315091c698a1a0b
Jesper Tjørnelund <69388371+tjorne@users.noreply.github.com>
2021-09-02 08:46:00 +0200

    Renamed scenes (added index).

--- Assets/Scripts/DistanceSnippet.cs
+++ Assets/Scripts/DistanceSnippet.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class DistanceSnippet : MonoBehaviour
 {
-
     public GameObject cube1;
     public GameObject cube2;
 
     void Update()
     {
+
+        // Note: 
+        // The Vector3.Distance is a somewhat performance heavy. Placing this in the update method is not good practice. 
+        // Alternatives could be InvokeRepeating and avoid the Vector3.Distance. 
+
         float distance = Vector3.Distance(cube1.transform.position, cube2.transform.position);
 
         Debug.Log(distance);
